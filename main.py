e0ef09e9ad593a567339e55e526e5cd752b8ea6b
Emirhan <72984140+lareithen@users.noreply.github.com>
2022-01-23 16:19:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from scrape import gorsel
+
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def anasayfa():
+  try:
+    x = gorsel()
+    print(x)
+    return render_template('index.html', resm = x)
+  except:
+    return anasayfa()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
+
+# larei was here
---
+++ scrape.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import cloudscraper, random, string
+
+def gorsel():
+    global rl
+    rastgele = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 6))
+    scraper = cloudscraper.create_scraper()
+    html = scraper.get(f'http://prnt.sc/{rastgele}').text
+    soup = BeautifulSoup(html, "html.parser")
+    resim_html = soup.find_all('img', {'class': 'no-click screenshot-image'})[0]['src']
+    s = "prntscr.com"
+    if s in resim_html:
+      return gorsel()
+    else:
+      return resim_html
+
+# larei was here.
